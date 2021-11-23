48fa41252fa0aa28cbed2f12ba95af0113a85c03
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-11-23 23:22:36 +0300

    P.R._student-card card_student__6

--- src/app/components/cardStudent.jsx
+++ src/app/components/cardStudent.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CardStudent = () => {
-    return <div>Card Student</div>;
+    const initialState = {
+        name: "",
+        surname: "",
+        yearOfBirth: "",
+        portfolio: ""
+    };
+    const [state, setState] = useState(initialState);
+    const handleChange = ({ target }) => {
+        console.log(target.id);
+        setState((prevState) => ({ ...prevState, [target.id]: target.value }));
+    };
+    return (
+        <form action="">
+            <div>
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={state.name}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="surname">Surname</label>
+                <input
+                    type="text"
+                    id="surname"
+                    value={state.surname}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="yearOfBirth">Year of birth</label>
+                <input
+                    type="text"
+                    id="yearOfBirth"
+                    value={state.yearOfBirth}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="portfolio">Portfolio</label>
+                <input
+                    type="text"
+                    id="portfolio"
+                    value={state.portfolio}
+                    onChange={handleChange}
+                />
+            </div>
+        </form>
+    );
 };
 
 export default CardStudent;
