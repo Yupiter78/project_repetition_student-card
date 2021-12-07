64a6d3a19157a0df1c3aa187ac8f11c53e702b91
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-12-07 23:58:30 +0300

    P.R._student-card code_refactoring_2__25

--- src/app/components/cardStudent.jsx
+++ src/app/components/cardStudent.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { getAge } from "../utils/utils";
+import { FIELDS } from "../utils/constants";
 
 const CardStudent = () => {
     const history = useHistory();
@@ -25,57 +26,89 @@ const CardStudent = () => {
     console.log("data:", data);
     console.log("isCreateUser:", isCreateUser);
 
+    const renderContent = (type, value) => {
+        return renderTemplate[value] || value;
+    };
+
+    const renderTemplate = {
+        year: (
+            <>
+                {data.yearOfBirth} ({getAge(data)})
+            </>
+        ),
+        portfolio: (
+            <a
+                href={data.portfolio}
+                target="_blank"
+                rel="noreferrer noopener"
+                className="fs-5 font-monospace link-primary"
+            >
+                {data.portfolio}
+            </a>
+        )
+    };
+
     return (
         <>
             {isCreateUser ? (
-                <div
-                    className="card mt-5 position-absolute start-50 translate-middle-x shadow"
-                    style={{ width: "28rem" }}
-                >
-                    <div className="card-header">Card student</div>
-                    <div className="card-body">
-                        <p>
-                            <span className="fs-5 fw-bold">Name: </span>
-                            <span className=" fs-5 font-monospace">
-                                {data.name}
-                            </span>
-                        </p>
-
-                        <p>
-                            <span className="fs-5 fw-bold">Surname: </span>
-                            <span className="fs-5 font-monospace">
-                                {data.surname}
-                            </span>
-                        </p>
-                        <p>
-                            <span className="fs-5 fw-bold">
-                                Year of birth:{" "}
-                            </span>
-                            <span className="fs-5 font-monospace">
-                                {data.yearOfBirth} ({getAge(data)})
-                            </span>
-                        </p>
-                        <p>
-                            <span className="fs-5 fw-bold">Portfolio: </span>
-                            <a
-                                href={data.portfolio}
-                                target="_blank"
-                                rel="noreferrer noopener"
-                                className="fs-5 font-monospace link-primary"
-                            >
-                                {data.portfolio}
-                            </a>
-                        </p>
-
-                        <button
-                            className="btn btn-primary mt-4 w-100"
-                            onClick={handleEdit}
-                        >
-                            Edit
-                        </button>
+                Object.keys(data).map((type) => (
+                    <div key={type}>
+                        <span className="fs-5 fw-bold">
+                            {FIELDS[type].label}:{" "}
+                        </span>
+                        <span className=" fs-5 font-monospace">
+                            {renderContent(type, data[type])}
+                        </span>
                     </div>
-                </div>
+                ))
             ) : (
+                // <div
+                //     className="card mt-5 position-absolute start-50 translate-middle-x shadow"
+                //     style={{ width: "28rem" }}
+                // >
+                //     <div className="card-header">Card student</div>
+                //     <div className="card-body">
+                //         <p>
+                //             <span className="fs-5 fw-bold">Name: </span>
+                //             <span className=" fs-5 font-monospace">
+                //                 {data.name}
+                //             </span>
+                //         </p>
+                //
+                //         <p>
+                //             <span className="fs-5 fw-bold">Surname: </span>
+                //             <span className="fs-5 font-monospace">
+                //                 {data.surname}
+                //             </span>
+                //         </p>
+                //         <p>
+                //             <span className="fs-5 fw-bold">
+                //                 Year of birth:{" "}
+                //             </span>
+                //             <span className="fs-5 font-monospace">
+                //                 {data.yearOfBirth} ({getAge(data)})
+                //             </span>
+                //         </p>
+                //         <p>
+                //             <span className="fs-5 fw-bold">Portfolio: </span>
+                //             <a
+                //                 href={data.portfolio}
+                //                 target="_blank"
+                //                 rel="noreferrer noopener"
+                //                 className="fs-5 font-monospace link-primary"
+                //             >
+                //                 {data.portfolio}
+                //             </a>
+                //         </p>
+                //
+                //         <button
+                //             className="btn btn-primary mt-4 w-100"
+                //             onClick={handleEdit}
+                //         >
+                //             Edit
+                //         </button>
+                //     </div>
+                // </div>
                 <div
                     className="card mt-5 position-absolute start-50 translate-middle-x shadow p-4"
                     style={{ width: "28rem" }}
---
+++ src/app/utils/constants.js
@@ -0,0 +1,18 @@
+export const FIELDS = {
+    name: {
+        label: "Name",
+        type: "text"
+    },
+    surname: {
+        label: "Surname",
+        type: "text"
+    },
+    year: {
+        label: "Year of birth",
+        type: "number"
+    },
+    portfolio: {
+        label: "Portfolio",
+        type: "text"
+    }
+};
